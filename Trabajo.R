63ec2dbe949448c5b9bb7090e9a7eb09267dc6e5
Jorge Molina Benavente <jormb2002@gmail.com>
2026-01-03 11:57:02 +0100

    Se ha actualizado el código, ahora es más limpio y funciona correctamente. Se ha añadido una nueva distribución, la mezcla bimodal simétrica centrada. Las simulaciones ahora tienen diferentes varianzas. Ahora se requiere el paquete mixtools para correr el script.

--- Trabajo.R
+++ Trabajo.R
@@ -1,150 +1,120 @@
-## Función que simule un set de datos con diferentes distribuciones
+##IMPORTANTE##
+# Hay que tener instalada la librería "mixtools"
+#install.packages("mixtools")
 
-simulacion <- function(n, distri, datos, varianza) {
+require("mixtools")
+
+## logaritmo_varianza - Función para obtener set aleatorios con la varianza y 
+#                       media que queramos de una distribución logarítmica
+#                       normal centrada
+
+# INPUTS - n (número de datos por set), delta (para obtener datos con medias
+#          diferentes) y varianza (para cambiar la varianza en un set)
+
+# OUTPUTS - dato (vector con un set de datos)
+
+logaritmo_varianza  <- function(n, varianza, delta) {
   
-# Elegir la distribución que queramos ("normal", "exponencial", "de la t" y "logarítmica")
+  datos <- rlnorm(n, 0, 1) - exp(0.5) 
+  # Se resta la media teórica exp(0.5) para obtener una media centrada en 0 y 
+  # poder comparar los errores tipo I
   
-  if (datos == 1) {  
-    datos1 <- rnorm (n, 0, varianza)
-    datos2 <- rnorm (n, distri, varianza)
-  } else if (datos == 2) {
-    datos1 <- rexp (n)
-    datos2 <- rexp (n) + distri
-  } else if (datos == 3) {
-    datos1 <- rt (n, df = 3)
-    datos2 <- rt (n, df = 3) + distri
-  } else if (datos == 4) {
-    datos1 <- rlnorm (n, 0, sdlog = varianza)
-    datos2 <- rlnorm (n, meanlog = distri, 1, sdlog = varianza)
-  }  
+  # Hacemos que la varianza sea 1 al dividir por la desviación teórica
+  num_estandar <- datos / sqrt((exp(1) - 1) * exp(1)) 
   
-  # Los p valores de cada test (test de la T, Wilcoxon y de normalidad)
-  
-  p_t <- t.test (datos1, datos2)$p.value
-  p_wil <- wilcox.test (datos1, datos2)$p.value
-  p_norm_1 <- shapiro.test(datos1)$p.value
-  p_norm_2 <- shapiro.test(datos2)$p.value
-  
-  # Devuelve TRUE si el p valor es mayor de 0.05  
-  
-  if (p_norm_1 > 0.05 && p_norm_2 > 0.05) {
-    p_final <- p_t
-    seleccion <- 1
-  } else {
-    p_final <- p_wil
-    seleccion <- 0
-  }
-  
-  resultado <- c (test_t = p_t < 0.05, test_wil = p_wil < 0.05, perverso = p_final < 0.05, hizo_t <- seleccion)
-  
-  # Da como resultado 3 columnas con TRUE o FALSE dependiendo del p-valor
-  
-  return(resultado)
+  # Obtenemos datos con la varianza^2 y media (delta) que indiquemos
+  dato <- num_estandar * varianza + delta
+ 
+ return(dato)
 }
 
-## Función que ejecuta la función anterior 10000 veces
 
-repeticiones <- function (n, delta = 0, distribucion = 1, varianza) {
+## logaritmo_varianza - Función para obtener set aleatorios con la varianza y 
+#                       media que queramos de una mezcla bimodal simétrica
+#                       centrada
+
+# INPUTS - n (número de datos por set), delta (para obtener datos con medias
+#          diferentes) y varianza (para cambiar la varianza en un set)
+
+# OUTPUTS - dato (vector con un set de datos)
+
+bimodal_varianza <- function(n, varianza, delta) {
   
-  resultados <- replicate (10000, simulacion (n, delta, distribucion, varianza))
+  # Del paquete mixtools
+  datos <- rnormmix (100, lambda = c(0.5, 0.5), mu = c(-1, 1), sigma = c(1, 1))
   
-  medias <- rowMeans(resultados)
+  # lambda c(0.5, 0.5) es para que sea simétrica
+  # mu y sigma controlan la media y la varianza, se deja en 1 ya que,
+  # normalizamos a media 0 y varianza 1
   
-  # Te da el error tipo I si delta = 0 o la potencia de cada test si deslta no es 0
+  # Normalizamos la varianza que es la suma de sigma^2 más mu^2 que siempre da 2
+  num_estandar <- datos / sqrt(2)
+  dato <- num_estandar * varianza + delta
   
-  elegir_t <- sum (resultados[4, ])
-  elegir_wil <- 10000 - elegir_t
-  
-  return(list(medias [1:3], elegir_t, elegir_wil))
 }
 
-## Función para iniciar el script
 
-inicio <- function() {
-  
-  datos <- as.numeric (readline("¿Cuántos datos por set quieres en tus simulaciones? "))
-  distribucion <- menu_distribucion ()
-  delta <- as.numeric (readline("Indica el número del delta "))
-  varianza <- menu_varianza ()
-  
-  repeticiones (datos, delta ,distribucion, varianza)
-}
+## simulación - Función que sirve para obtener datos aleatiros de diferentes
+#               distribuciones y hacer las simulaciones con el método perverso
 
-## Función qué sirve como menú para elegir la distribución
+# INPUTS - n (número de datos por set), delta (para obtener datos con medias
+#          diferentes), distribucion (seleccionar el tipo de distribución) y 
+#          varianza (para cambiar la varianza en un set)
 
-menu_distribucion <- function() {
-  
-  # Se muestran las opciones
-  
-  cat ("¿Qué distribución quieres?\n")
-  cat ("1 - Normal\n")
-  cat ("2 - Exponencial\n")
-  cat ("3 - de la T con 3 grados de libertad\n")
-  cat ("4 - Logarítmica\n")
-  
-  # Se elige la opción
-  
-  opcion <- as.integer ((readline ("¿Qué opción quieres? ")))
-  
-  return(opcion)
-}
+# OUTPUTS - Vector con 4 resultados, los 3 primeros pueden ser TRUE/FALSE
+#           e indican si el test de la T, Wilcoxon y los de normalidad han dado
+#           valores p menores a 0.05, el último puede ser 1/0 dependiendo si se
+#           escogido o no el test de la T
 
-## Igual pero para cambiar la varianza
-
-menu_varianza <- function() {
-  opcion <- readline("¿Quieres cambiar la varianza?\ny/n\n")
-  
-  if (opcion == "y") {
-    vari <- as.numeric (readline("Valor de la varianza "))
-  } else {
-    vari <- 1 # Si se dice que no, varianza = 1
-  }
-  
-  return(vari)
-}
-
-inicio ()
-
-
-
-####CODIGO ACTUALIZADO:
-
-#FUNCION DE SIMULACION CORREGIDA
-#Distribuciones no centradas.
-
-
-
-simulacion <- function(n, delta, distribucion) {   #se cambia nombre de argumento a delta, para que refleje tamaño del efecto
-  if (distribucion == 1) {            # Normal
-    x <- rnorm(n, 0, 1)               # delta = 0 ambas muestras misma distribución
-    y <- rnorm(n, delta, 1)           # delta ≠ 0 diferencia clara de localizacion
+simulacion <- function(n, delta, distribucion, varianza) {   
+  if (distribucion == 1) {  # Normal
+    x <- rnorm(n, 0, 1)     # Si delta = 0 ambas muestras misma distribución
+    y <- rnorm(n, delta, varianza) # Si delta ≠ 0, las medias difieren 
     
-  } else if (distribucion == 2) {     # Exponencial centrada
-    x <- rexp(n) - 1                  #se resta la media teorica (1) para que este centrada. Si no está centrada, aun con delta = 0  el error tipo I no es interpretable.
-    y <- rexp(n) - 1 + delta
+  } else if (distribucion == 2) { # Exponencial centrada
+    # Se resta la media teorica (1) para que este centrada. Si no está centrada, 
+    # aun con delta = 0  el error tipo I no es interpretable
     
-  } else if (distribucion == 3) {     # t(df=3) escalada a varianza 1. hace comprarables tamaños de efecto entre distribuciones y evita que diferencias de escala influyan en la potencia.
-    x <- rt(n, 3) / sqrt(3)
-    y <- rt(n, 3) / sqrt(3) + delta
+    x <- rexp(n) - 1                
+    y <- (rexp(n) - 1) * varianza + delta
+    
+  } else if (distribucion == 3) {# Distribución de la T con 3 grados de libertad
+    # La varianza es 1, para hacer comprarables los tamaños de efecto entre 
+    # distribuciones y evitar que diferencias de escala influyan en la potencia
+    
+    x <- rt(n, 3) / sqrt(3) # Normalizamos a varianza = 1
+    y <- (rt(n, 3) / sqrt(3)) * varianza  + delta
     
   } else if (distribucion == 4) {     # Log-normal centrada
-    x <- rlnorm(n, 0, 1) - exp(0.5)   # se resta la media teorica exp(0.5). Mismo razonamiento que en la exponencial
-    y <- rlnorm(n, 0, 1) - exp(0.5) + delta
+    x <- logaritmo_varianza(n, 1, 0)
+    y <- logaritmo_varianza(n, varianza, delta)
+    
+  } else if (distribucion == 5) {     # Mezcla bimodal simétrica centrada
+    x <- bimodal_varianza(n, 1, 0)
+    y <- bimodal_varianza(n, varianza, delta)
+    
   }
   
-  p_t <- t.test(x, y, var.equal = FALSE)$p.value   # antes se asumia varianzas iguales. Ahora Welch ( var.equal = FALSE). Mas robusto cuando varianzas difieren
-  p_w <- wilcox.test(x, y, exact = FALSE)$p.value
+  p_t <- t.test(x, y)$p.value   # Se hace test de Welch
+  p_w <- wilcox.test(x, y, exact = FALSE)$p.value 
+  # exact = FALSE para que el cálculo sea más rápido
   
+  # Hacemos los test de normalidad
   p_norm_x <- shapiro.test(x)$p.value
   p_norm_y <- shapiro.test(y)$p.value
   
+  # Elegimos test de la T, cuando ambos valores p de los test de normalidad 
+  # son menores a 0.05
+  
   if (p_norm_x > 0.05 && p_norm_y > 0.05) {
     p_p <- p_t
-    chose_t <- 1  # se indica explicitamente que se usó el t-test
+    chose_t <- 1  # Si se elige el test de la t, la variable toma valor 1
   } else {
-    p_p <- p_w
+    p_p <- p_w # Si se elige Wilcoxon, la  variable toma valor 0
     chose_t <- 0
   }
+  
+  # Los resultados devuelven TRUE/FALSE y 1/0
   
   c(
     t_test = p_t < 0.05,
@@ -155,56 +125,103 @@ simulacion <- function(n, delta, distribucion) {   #se cambia nombre de argument
 }
 
 
-##REPETICIONES DE MONTE CARLO
+## repeticiones - Para ejecutar la función simulación muchas veces´
 
-repeticiones <- function(n, delta, distribucion, B = 10000) {
+# INPUTS - n (número de datos por set), delta (para obtener datos con medias
+#          diferentes), distribucion (seleccionar el tipo de distribución), 
+#          varianza (para cambiar la varianza en un set) y B (número de veces
+#          que se ejecuta la función simulación)
+
+# OUTPUTS - Una media de cuantas veces ha salido TRUE en el test de la T,
+#           Wilcoxon y el procedimiento perverso y el último, probabilidad de 
+#           elegir el test de la T según el procedimiento
+
+repeticiones <- function(n, delta, distribucion, varianza, B = 10000) {
   
-  resultado <- replicate(B, simulacion(n, delta, distribucion))
-  colMeans(resultado)
+  resultado <- replicate(B, simulacion(n, delta, distribucion, varianza))
+  
+  rowMeans(resultado)
 }
 
 
-#BUVLES DE TAMAÑOS MUESTRALES     
+## inicio - Función para iniciar el script
 
-ns <- c(10, 20, 50, 100)
-deltas <- c(0, 0.2, 0.5)
-distribuciones <- 1:4
+# OUTPUTS - Data frame con los resultados de las diferentes simulaciones
 
-resultados <- data.frame()
+inicio <- function() {
+  datos <- c(10, 20, 50, 100)
+  deltas <- c(0, 0.2, 0.5)
+  tipo_distribucion <- 1:5
+  varianza <- c(1, 2, 4)
+  
+  resultado <- calculo_resultados (distribuciones = tipo_distribucion, 
+                                   deltas = deltas, ns = datos, 
+                                   varianzas = varianza)
+  
+  return(resultado)
+}
 
-for (d in distribuciones) {    #se evita ejecucion interactiva para qie las simulaciones sean reproducibles y no se produzcan errores por interaccion manual
-  for (n in ns) {
-    for (delta in deltas) {
-      
-      tasas <- repeticiones(n, delta, d)
-      
-      resultados <- rbind(
-        resultados,
-        data.frame(
-          distribucion = d,
-          n = n,
-          delta = delta,
-          metodo = c("t_test", "wilcoxon", "perverso"),
-          tasa = tasas[1:3],
-          prob_elegir_t = tasas["chose_t"]
-        )
-      )
+
+## calculo_resultados - Para obtener los resultados de las simulaciones
+
+# INPUTS - n (número de datos por set), delta (para obtener datos con medias
+#          diferentes), distribucion (seleccionar el tipo de distribución) y
+#          varianza (para cambiar la varianza en un set)
+
+# OUTPUTS - Data frame con los resultados de las diferentes simulaciones
+
+calculo_resultados <- function(distribuciones, ns, deltas, varianzas) {
+  
+  resultados <- data.frame()
+  
+  # Hacemos varios bucles para que haga todas las posibles simulaciones
+  
+  for (d in distribuciones) { 
+    for (n in ns) {
+      for (delta in deltas) {
+        for (v in varianzas) {
+          tasas <- repeticiones(n, delta, d, v)
+        
+          resultados <- rbind(
+            resultados,
+            data.frame(
+              distribucion = d,
+              n = n,
+              delta = delta,
+              metodo = c("t_test", "wilcoxon", "perverso"),
+              varianza = v^2,
+              tasa = tasas[1:3],
+              prob_elegir_t = tasas["chose_t"]
+            )
+          )
+        }
+      }
     }
   }
+  return(resultados) # Resultados debe tener 540 objetos
 }
 
-head(resultados)   #para mirar el objeto resultados. Aparece data.frame 
-nrow(resultados)    #COMPROBACIÓN IMPORTANTE. DEbe dar 144. 4 distribuciones*4n*3deltas*3metodos
+## visualizar - Para ver resultados específicos
 
-#ERROR TIPO I vs POTENCIA
-subset(resultados, delta == 0) # delta == 0. No hay diferencia real entre grupos. tasa = error tipo I empirico. (Valor "correcto": aprox 0,05)
-#Interpretación típica: “Bajo la hipótesis nula, el procedimiento perverso no mantiene siempre el nivel nominal del 5%, y su comportamiento depende del tamaño muestral y de la distribución.”
+# INPUTS - resultados (data frame obtenido de calculo_resultados)
 
-subset(resultados, delta == 0.5) # detla > 0. Si hay diferencia real. tasa =potencia. Cuanto mayor, mejor
-# Interpretación típica:“Para un efecto moderado, el procedimiento perverso suele mostrar menor potencia que el mejor de los métodos fijos, debido a la selección basada en el test de normalidad.”
+# OUTPUTS - 3 subsets, con el error tipo I (delta = 0), potencia (delta = 0.5)
+#           procedimiento perverso (metodo = "perverso")
 
-subset(resultados, metodo == "perverso") #columna prob_elegir_t es fundamental para la discusion
-# interpretación: Valores cercanos a 1: casi siempre t test  ///////  Valores cercanos a 0: casi siempre Wilcoxon
-# Para n pequeño: prob_elegir_t alta //////Para n grande: prob_elegir_t baja (sobre todo en distribuciones no normales()
-# Conclusión típica: “El criterio de selección depende fuertemente del tamaño muestral, no solo de la forma de la distribución.”
+visualizar <- function(resultados) {
+  
+  # Error Tipo I vs Potencia
+  
+  subset(resultados, delta == 0) 
+  # delta == 0 - No hay diferencia real entre grupos, 
+  # tasa = error tipo I empirico (Valor "correcto": aprox 0.05)
+  
+  subset(resultados, delta == 0.5) 
+  # detla > 0 - Hay diferencia real, tasa = potencia (Cuanto mayor, mejor)
+  
+  # Solo las tasas del método perverso
+  subset(resultados, metodo == "perverso") 
+  
+}
 
+resultados <- inicio()
